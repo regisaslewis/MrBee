d7adae66626ffb7b833d3d72e8bbf4faaf142395
RegisLewis <regisaslewis@gmail.com>
2025-01-26 14:09:12 -0800

    test for word_check and player_guess

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ def word_check(word):
         return f"'{word}' has too few unique letters."
     if len(letters) > 7:
         return f"'{word}' has too many unique letters."
-    return f"Acceptable word: '{word}'"
+    return "Acceptable word."
 
 def player_guess(word, guess):
     word_letters = [n for n in word]
     guess_letters = [n for n in guess]
     unusable_letters = [n for n in guess_letters if n not in word_letters]
     if unusable_letters:
-        return f"Unusuable letters: {sorted(set(unusable_letters), key=unusable_letters.index)}"
-    return f"{guess}: Success!"
+        return sorted(set(unusable_letters), key=unusable_letters.index)
+    return "Success!"
 
 
 print("word_check:", word_check("hexagon"))
