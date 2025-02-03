39b2a3d4922a39391ae546c89cd890d9cf310d17
RegisLewis <regisaslewis@gmail.com>
2025-02-02 21:55:47 -0800

    add list of words and test

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Moved to cli.py")
+from all_words import word_list
+
+word = "alphabet"
+
+if word in word_list:
+    print(f"{word} found at position {word_list.index(word)}")
+else:
+    print("Word not found.")
