0390feb598fe1d9af3954e6d67311fbd229d0fa3
RegisLewis <regisaslewis@gmail.com>
2025-02-18 04:23:37 -0800

    add prompt collection

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,5 +1,6 @@
 from all_words import word_dictionary
 import random
+import datetime
 
 prompt_list = [n for n in word_dictionary if len(set(n)) == 7]
 
@@ -7,3 +8,13 @@ def generate_prompt():
     prompt = random.choice(prompt_list)
 
     return prompt
+
+today = datetime.date.today()
+prompt_dictionary = {}
+
+for n in range(30):
+    sample_letters = sorted(set([n for n in generate_prompt()]))
+    random.shuffle(sample_letters)
+    prompt_dictionary.update({(today + datetime.timedelta(days = n)).strftime("%Y-%m-%d"): sample_letters})
+
+print(prompt_dictionary)
