5d240f1f9e8f28088ad492a90c80d20713c6f170
RegisLewis <regisaslewis@gmail.com>
2025-02-20 22:31:24 -0800

    revert cli.py and player_guess.py and add pickle versions

---
+++ cli_pickle.py
@@ -0,0 +1,51 @@
+import random
+from player_guess_pickle import player_guess, show_points, read_guesses, today
+from results import find_words, find_pangrams, find_points
+from prompt_collection import prompt_collection
+
+def main():
+
+    sample_letters = prompt_collection.get(today)
+    necessary_letter = sample_letters[0]
+    optional_letters = [n for n in sample_letters if n != necessary_letter]
+    possible_points = find_points(necessary_letter, sample_letters)
+    win_condition = 100 if possible_points > 300 else (possible_points / 3)
+    grats_unshown = True
+    today_guesses = read_guesses()[today]
+
+    print(f"Letters for {today}: |{necessary_letter.upper()}| {optional_letters}")
+    print(f"Total Possible Points: {possible_points}")
+    print(f"Points to Genius: {win_condition}")
+    print("Enter 1 to see your words.")
+    print("Enter 2 to shuffle.")
+    print("Enter 0 to quit.")
+    print("=============")
+    while True:
+        player_choice = input("Enter a word: ")    
+        if player_choice == "0":
+            print("Good Bye!")
+            exit()
+        elif player_choice == "1":
+            print(f"{show_points()} of {win_condition} points in {len(today_guesses)} word{'' if len(today_guesses) == 1 else 's'}:")
+            print("None yet" if len(today_guesses) == 0 else sorted(today_guesses))
+            print("=============")
+            print(f"|{necessary_letter.upper()}| {optional_letters}")
+        elif player_choice == "2":
+            random.shuffle(optional_letters)
+            print("Shuffling...")
+            print("=============")
+            print(f"|{necessary_letter.upper()}| {optional_letters}")
+        elif player_choice == "54532":
+            print(find_words(necessary_letter, sample_letters))
+            print(find_pangrams(necessary_letter, sample_letters))
+        else:
+            player_guess(sample_letters, necessary_letter, player_choice.lower())
+            print("=============")
+            print(f"|{necessary_letter.upper()}| {optional_letters}")
+        if show_points() >= win_condition and grats_unshown:
+            print("We have ourselves a genius!")
+            print("Enter 0 to finish or keep going!")
+            grats_unshown = False
+
+if __name__ == "__main__":
+    main()
---
+++ player_guess_pickle.py
@@ -0,0 +1,62 @@
+from datetime import date
+import pickle
+
+from all_words import word_dictionary
+today = date.today().strftime("%Y-%m-%d")
+
+def read_guesses():
+    with open("successful_guesses.pkl", "rb") as f:
+        while True:
+            try:
+                data = pickle.load(f)
+            except EOFError:
+                break
+        return data
+    
+successful_guesses = read_guesses() 
+
+def update_sg():
+    with open("successful_guesses.pkl", "ab") as f:
+        pickle.dump(successful_guesses, f)
+
+def player_guess(word, necessary_letter, guess):
+    word_letters = [n for n in word]
+    guess_letters = [n for n in guess]
+    unusable_letters = [n for n in guess_letters if n not in word_letters]
+    if unusable_letters:
+        print(f"Unacceptable letters: {sorted(set(unusable_letters), key=unusable_letters.index)}")
+        return False
+    if necessary_letter not in guess:
+        print(f"Necessary letter |{necessary_letter.upper()}| not used in '{guess}.')")
+        return False
+    if len(guess) < 4:
+        print(f"{guess}: Too few letters provided.")
+        return False
+    if guess not in word_dictionary:
+        print(f"'{guess}' is not found in our dictionary.")
+        return False
+    if guess in successful_guesses[today]:
+        print(f"'{guess}' was already used.")
+        return False
+    if len(set(guess)) == 7:
+        update_sg()
+        successful_guesses[today].append(guess)
+        print(f"{guess}: Now that's a PANGRAM!!")
+        print(f"Your Points: {show_points()}")
+        return True
+    update_sg()
+    successful_guesses[today].append(guess)
+    print(f"{guess}: Success!")
+    print(f"Your Points: {show_points()}")
+    return True
+
+def show_points():
+    points = 0
+    for n in successful_guesses[today]:
+        if len(set(n)) == 7:
+            points += (7 + len(n))
+        elif len(n) > 4:
+            points += len(n)
+        else:
+            points += 1
+    return points
