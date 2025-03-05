e286d2bbb7a440ec987ded76c2dc559a6006dab3
RegisLewis <regisaslewis@gmail.com>
2025-03-04 21:43:37 -0800

    add CHEAT STUFF to cli_json.py

--- cli_json.py
+++ cli_json.py
@@ -10,6 +10,7 @@ def main():
     possible_points = find_points(necessary_letter, sample_letters)
     win_condition = 100 if possible_points > 300 else (possible_points / 3)
     grats_unshown = True
+    cheated = False
 
     print(f"Letters for {today}: |{necessary_letter.upper()}| {optional_letters}")
     print(f"Total Possible Points: {possible_points}")
@@ -18,11 +19,15 @@ def main():
     print("Enter 1 to see your words.")
     print("Enter 2 to shuffle.")
     print("Enter 0 to quit.")
-    print("=============")
     while True:
+        print("=============")
+        print(f"|{necessary_letter.upper()}| {optional_letters}")
         player_choice = input("Enter a word: ")    
         if player_choice == "0":
-            print("Good Bye!")
+            if cheated:
+                print("Good Bye, CHEATER!")
+            else:
+                print("Good Bye!")
             exit()
         elif player_choice == "1":
             print(f"{show_points()} of {win_condition} points in {len(read_guesses()[today])} word{'' if len(read_guesses()[today]) == 1 else 's'}:")
@@ -35,8 +40,30 @@ def main():
             print("=============")
             print(f"|{necessary_letter.upper()}| {optional_letters}")
         elif player_choice == "54532":
-            print(find_words(necessary_letter, sample_letters))
-            print(find_pangrams(necessary_letter, sample_letters))
+            cheated = True
+            print("*********************")
+            print("Welcome to cheating!")
+            print("Enter 1 for today's prompt.")
+            print("Or enter your own prompt")
+            print("(the first letter will be the necessary one)")
+            print("(no)")
+            print("Enter 0 to return to the game.")
+            print("*********************")
+            answers_search = input("Enter prompt: ")
+            if answers_search == "1":
+                print(f"Words: {find_words(necessary_letter, sample_letters)}")
+                print(f"Pangrams: {find_pangrams(necessary_letter, sample_letters)}")
+            elif answers_search == "0":
+                pass
+            else:
+                cheat_prompt = [n for n in answers_search.lower()]
+                if len(set(cheat_prompt)) == 7:
+                    print(f"Words: {find_words(cheat_prompt[0], cheat_prompt)}")
+                    print(f"Pangrams: {find_pangrams(cheat_prompt[0], cheat_prompt)}")
+                    print(f"Total possible points: {find_points(cheat_prompt[0], cheat_prompt)}")
+                else:
+                    print("Not 7 valid letters.")
+            print("Returning to the game...")
         else:
             player_guess(sample_letters, necessary_letter, player_choice.lower())
             print("=============")
